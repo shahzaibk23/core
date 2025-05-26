f2b084bdc08548a816084a15531233b874ed94dd
shahzaibk23 <shahzaibceo@gmail.com>
2025-05-27 03:25:36 +0500

    SRAM Test update

--- src/test/scala/components/SramTop_test.scala
+++ src/test/scala/components/SramTop_test.scala
@@ -26,16 +26,18 @@ class SRamTop_test extends AnyFreeSpec with Matchers with ChiselSim
         val settings = Backend.CompilationSettings(
             timing = Some(Backend.CompilationSettings.Timing.TimingDisabled),
         )
-        simulate(new SRamTop(Some("instrx.hex")))
+        simulate(new SRamTopDuo(Some("/Users/shahzaibkashif/core/instrx.hex")))
         {
             dut =>
                 dut.io.req.valid.poke(1.B)
-                dut.io.req.bits.dataRequest.poke("h1111111333333313".U)
-                dut.io.req.bits.addrRequest.poke(4.U)
-                dut.io.req.bits.activeByteLane.poke("b11111111".U)
-                dut.io.req.bits.isWrite.poke(1.B)
-                dut.clock.step(3)
+                dut.io.req.bits.dataRequest.poke(0.U)
+                dut.io.req.bits.addrRequest.poke(1.U)
+                dut.io.req.bits.activeByteLane.poke("b1111".U)
                 dut.io.req.bits.isWrite.poke(0.B)
+                dut.clock.step(3)
+                dut.io.req.bits.addrRequest.poke(0.U)
+                dut.clock.step(3)
+                dut.io.req.bits.addrRequest.poke(2.U)
                 dut.clock.step(3)
                 
         }
