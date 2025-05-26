c47be64cc7bb43293e3af1452d570fed8a5df8f7
shahzaibk23 <shahzaibceo@gmail.com>
2025-05-27 03:26:25 +0500

    create IF Stage

--- src/main/scala/pipeline/IF.scala
+++ src/main/scala/pipeline/IF.scala
@@ -1,13 +1,50 @@
 package pipeline
 
-import common.Component
-import components.{PC}
+import chisel3._
+import chisel3.util._
+
+import common.{Component, MemRequestIO, MemResponseIO}
+import components.{PC, InstructionFetch}
+
+class InstructionFetchStage_Ins(pc_width: Int) extends Bundle
+{
+    val pc  :   UInt    =   UInt(pc_width.W)
+}
+
+class InstructionFetchStage_CoreTop_Out(dw:Int, mw:Int) extends Bundle
+{
+    val imemReq:    DecoupledIO[MemRequestIO]   =   Decoupled(new MemRequestIO(dw, mw))
+    val imemRsp:    DecoupledIO[MemResponseIO]  =   Flipped(Decoupled(new MemResponseIO(dw)))
+}
+
+class InstructionFetchStage_IO(data_width: Int, pc_width: Int) extends Bundle
+{
+    // val in  =   Input(new InstructionFetchStage_Ins(pc_width))
+    val if_id =   Output(new IF_ID(pc_width))
+    val core_out =   new InstructionFetchStage_CoreTop_Out(data_width, pc_width)
+}
 
 class InstructionFetchStage extends Component
 {
-    val io = IO(new IF_ID)
+    val dataWidth: Int  = 32
+    val pc_width: Int   = log2Ceil(config.ImemSize)
 
-    // val PC = Module(new PC).io
+    val io = IO(new InstructionFetchStage_IO(dataWidth, pc_width))
+
+    val PC = Module(new PC).io
+    val IF = Module(new InstructionFetch).io
+
+    PC.halt := 0.B // not wired a.t.m
+    PC.in   := PC.pc4
+
+    IF.address := PC.in.asUInt
+    IF.stall   := 0.B
+
+    io.core_out.imemReq <> IF.coreInstrReq
+    IF.coreInstrRsp     <> io.core_out.imemRsp
+
+    io.if_id.instruction  := IF.instruction
+    io.if_id.pc           := PC.pc
 
 
 }
