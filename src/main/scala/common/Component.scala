dd4b5e60be17d488e0090cfdd867d2c3b2aa76d2
shahzaibk23 <shahzaibceo@gmail.com>
2025-06-02 05:30:57 +0500

    create ComponentIO abstract bundle

--- src/main/scala/common/Component.scala
+++ src/main/scala/common/Component.scala
@@ -1,6 +1,11 @@
 package common
 
 import chisel3._
-import configs.BaseConfig
+import configs.CoreConfig
 
-abstract class Component(implicit val config:BaseConfig) extends Module
+abstract class Component(implicit val config:CoreConfig) extends Module
+abstract class ComponentIO(implicit val config: CoreConfig) extends Bundle
+{
+    val dw:     Int = config.XLEN
+    val mw:     Int = config.ImemSize
+}
