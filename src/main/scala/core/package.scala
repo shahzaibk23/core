abcfc9ad50cddd628e07d75644eeeb479287ee7d
shahzaibk23 <shahzaibceo@gmail.com>
2025-06-02 05:33:10 +0500

    update core package object

--- src/main/scala/core/package.scala
+++ src/main/scala/core/package.scala
@@ -1,5 +1,5 @@
 package object core
 {
   // Default implicit BaseConfig for all components
-  implicit val defaultConfig: configs.BaseConfig = configs.BaseConfig(ImemSize = 1024) // Adjust ImemSize as needed
+  implicit val defaultConfig: configs.TracerConfig = configs.SingleCoreWithTracer() // Adjust ImemSize as needed
 }
