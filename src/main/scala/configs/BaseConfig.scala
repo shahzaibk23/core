c2d1a1a42a49e61b2217902bbb6c1305ad11c8bf
shahzaibk23 <shahzaibceo@gmail.com>
2025-06-02 05:31:31 +0500

    BaseConfig -> CoreConfig

--- src/main/scala/configs/BaseConfig.scala
+++ src/main/scala/configs/BaseConfig.scala
@@ -1,7 +1,12 @@
 package configs
 
-case class BaseConfig
-(
-    val ISA     : Int = 64,
+trait CoreConfig
+{
+    val ISA     : Int = 64
     val ImemSize: Int = 1024 // 1KB -- in bytes
-)
+    val XLEN:     Int = ISA
+
+    val hasTracer: Boolean = false
+}
+
+//TODO: at some point if needed convert this to a trait//
