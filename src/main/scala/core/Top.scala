35858043f84495c607f1ff3859077099c2552677
shahzaibk23 <shahzaibceo@gmail.com>
2025-06-02 05:34:08 +0500

    instantiate Tracer & attach rvfi_o to Top boundary

--- src/main/scala/core/Top.scala
+++ src/main/scala/core/Top.scala
@@ -3,20 +3,29 @@ package core
 import chisel3._
 import chisel3.util._
 
-import common.Component
+import common.{Component, ComponentIO}
 
 import components.{SRamTopUno, SRamTopDuo}
+import uncore.{Tracer, TracerO}
+import configs.TracerConfig
 
-class Top_IO(dw:Int) extends Bundle
+class Top_IO extends ComponentIO
 {
     val pin: UInt = Output(UInt(dw.W))
+
+    // val rvfi: Option[TracerO] = if (config.hasTracer) Some(new TracerO(config.NRET, dw)) else None
+    val rvfi: Option[TracerO] = config match {
+        case tc: TracerConfig if config.hasTracer => Some(new TracerO(tc.NRET, dw))
+        case _ => None
+    }
+
 }
 
 class Top extends Component
 {
     val dataWidth: Int = config.ISA
 
-    val io = IO(new Top_IO(dataWidth))
+    val io = IO(new Top_IO)
 
     val IMEM = Module(new SRamTopUno(Some("/Users/shahzaibkashif/core/instrx.hex"))).io
 
@@ -33,4 +42,11 @@ class Top extends Component
     io.pin <> CORE.pin
 
     CORE.stall := 0.B
+
+    if (config.hasTracer)
+    {
+        val TRACER = Module(new Tracer)
+        TRACER.rvfi_i <> CORE.rvfi.get
+        io.rvfi.get <> TRACER.rvfi_o
+    }
 }
