685ca8c52eebb5142092c94bee42d245112e49b4
shahzaibk23 <shahzaibceo@gmail.com>
2025-06-30 21:54:13 +0000

    fix ComponentsIO mw

--- src/main/scala/common/Component.scala
+++ src/main/scala/common/Component.scala
@@ -1,11 +1,12 @@
 package common
 
 import chisel3._
+import chisel3.util._
 import configs.CoreConfig
 
 abstract class Component(implicit val config:CoreConfig) extends Module
 abstract class ComponentIO(implicit val config: CoreConfig) extends Bundle
 {
     val dw:     Int = config.XLEN
-    val mw:     Int = config.ImemSize
+    val mw:     Int = log2Ceil(config.ImemSize)
 }
